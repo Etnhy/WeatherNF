659a1bef7b43f0e73da31bc9d0037afceb975743
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-03-18 21:43:07 +0200

    presenter fix

--- WeatherNF/Modules/Main/MainPresenter.swift
+++ WeatherNF/Modules/Main/MainPresenter.swift
@@ -39,7 +39,7 @@ class MainPresenter: MainViewPresenterProtocol {
     required init(view: MainViewProtocol, networkManager: NetworkManager) {
         self.view = view
         self.networkManager = networkManager
-//        setWeather()
+        setWeather()
         setWeatherWithCityName()
     }
     
@@ -59,29 +59,18 @@ class MainPresenter: MainViewPresenterProtocol {
     
     func setWeatherWithCityName() {
         
-//        NetworkManager.shared.getWeatherWithName(cityName: "Kyiv")
-//            .observe(on: MainScheduler.instance)
-//            .subscribe { [weak self] items in
-//                guard let self = self else { return }
-//                self.cityWeather = items
-////                guard let weatherModel = self.weatherModel else {return}
-////                self.view?.setWeather(model: weatherModel)
+        NetworkManager.shared.getWeatherWithName(cityName: "Kyiv")
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] items in
+                guard let self = self else { return }
+                self.cityWeather = items
+                guard var cityWeather = self.cityWeather else { return }
+                self.view?.setWeatherCity(cityModel: cityWeather)
 //                print(self.cityWeather)
-//            } onError: { error in
-//                print(error)
-//            }.disposed(by: dispose)
+            } onError: { error in
+                print(error)
+            }.disposed(by: dispose)
         
-//        NetworkManager.shared.getWeatherWithLatLon()
-//            .observe(on: MainScheduler.instance)
-//            .subscribe { [weak self] items in
-//                guard let self = self else { return }
-//                self.cityWeather = items
-//                guard let cityWeather = self.cityWeather else {return}
-//                self.view?.setWeatherCity(cityModel: cityWeather)
-//            } onError: { error in
-//                print(error)
-//            }.disposed(by: dispose)
-
     }
     
     func setWeatherLatLon(latitude: Double, longitude: Double) {
